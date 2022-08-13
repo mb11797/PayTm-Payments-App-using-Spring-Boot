575ccca57f4abd3d9b50d3c4a4a6a42045328bda
Manas Bhardwaj <mb.manasbhardwaj@gmail.com>
2022-08-13 15:28:00 +0530

    paytm-payments-app

---
+++ src/main/java/com/tom/paytmpaymentsapp/PaytmPaymentsAppApplication.java
@@ -0,0 +1,13 @@
+package com.tom.paytmpaymentsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytmPaymentsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaytmPaymentsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tom/paytmpaymentsapp/PaytmPaymentsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom.paytmpaymentsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytmPaymentsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
